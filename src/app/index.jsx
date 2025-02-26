c549342d5457a8dfd57de7ca10fba30440ae56d8
otavioadamis <otavioadamis@gmail.com>
2025-02-26 16:25:29 -0300

    criado tela de cadastro

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,9 +1,11 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Link } from 'expo-router'
 
 const Home = () => {
   return (
     <View>
+      <Link href="signup">Signup</Link>
       <Text>Home</Text>
     </View>
   )
