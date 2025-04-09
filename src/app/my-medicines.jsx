99862227a3220ee57e17359df167f177d9b44026
Sofia Ximenes <sofiaantonacio@gmail.com>
2025-04-09 14:00:38 -0300

    melhorando o layout

--- src/app/my-medicines.jsx
+++ src/app/my-medicines.jsx
@@ -1,20 +1,25 @@
-import { View, Text, TextInput, Image, TouchableOpacity, KeyboardAvoidingView, ScrollView, Platform } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import Fontisto from '@expo/vector-icons/Fontisto';
-import Slider from '../components/Slider';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
-import Card from '../components/Card';
+import { View, Text, SafeAreaView } from 'react-native'
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 
 const MyMedicines = () => {
-    return (
-            <View className='p-2'>
-              <Text>Inicio</Text>
-                <Card></Card>
+  return (
+    <SafeAreaView>
+        <View className='flex-row gap-x-4 p-4 items-center bg-slate-200'>
+            <AntDesign name="arrowleft" size={24} color="black" />
+            <View className='flex-col'>
+                <Text className='font-bold text-xl'>Meus Medicamentos</Text>
             </View>
-          )
+        </View>
+        <View className='flex-col p-4 gap-y-4 bg-cyan-500'>
+            <Text className='text-white font-medium text-2xl'>Nome medicamento</Text>
+            <Text className='text-white font-medium text-xl'>Tipo remedio</Text>
+            <Text className='text-white font-medium text-xl'>Necessita receita:</Text>
+        </View>
+    </SafeAreaView>
+)
 }
 
 export default MyMedicines
+
